cb75a19bb9f709ef299091cfe0fee298c4c79644
Manon Lan <manondoublier@yahoo.fr>
2018-08-27 18:08:00 +0200

    ajout validation uniqueness

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   has_many :carts
 
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness:true
   validates :description, presence:true
   validates :price, presence:true
   validates :image_url, presence:true
