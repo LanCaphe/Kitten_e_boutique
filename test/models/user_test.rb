b5a5138ed50421a91ef68b6f74cbe8acb1b0b8fc
ticho <tibofolly@gmail.com>
2018-08-27 18:23:33 +0200

    added 1 model test, passed

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    User.destroy_all
+    @user = User.new(
+      username: 'tata',
+      email: 'tata@lol.com',
+      password: 'qwerty',
+      password_confirmation: 'qwerty'
+    )
+  end
+
+  test "can create a valid user" do
+    assert_not @user.errors.any?
+    # assert users(:one)
+  end
 end
