bd947c2d7e973e82ae7be297aa7eeb6e2ff442eb
ticho <tibofolly@gmail.com>
2018-08-27 18:40:58 +0200

    added some tests on User model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -9,10 +9,34 @@ class UserTest < ActiveSupport::TestCase
       password: 'qwerty',
       password_confirmation: 'qwerty'
     )
+    @user.save
   end
 
   test "can create a valid user" do
     assert_not @user.errors.any?
-    # assert users(:one)
+  end
+
+  test "invalid user can't be created" do
+    @user.email = nil
+    @user.save
+    assert @user.errors.any?
+  end
+
+  test "username is required" do
+    @user.username = nil
+    @user.save
+    assert @user.errors.any?
+  end
+
+  test "username must be at least 3char long" do
+    @user.username = ''
+    @user.save
+    assert @user.errors.any?
+  end
+
+  test "username cant be longer than 10char" do
+    @user.username = '123456789011'
+    @user.save
+    assert @user.errors.any?
   end
 end
