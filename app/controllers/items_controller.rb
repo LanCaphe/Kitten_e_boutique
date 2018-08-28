9596e76641dd3b278a0cb65ba081b12d889f5a4a
lariskau <bougaut.lea@gmail.com>
2018-08-28 15:13:16 +0200

    item show back and basic front added

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,5 +3,11 @@ class ItemsController < ApplicationController
   end
 
   def show
+    begin
+      @item = Item.find(params[:id])
+    rescue StandardError => e
+      p e.message
+    end
+    redirect_to root_path if !@item
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root 'items#index'
-  get 'items/show', to: 'items#show'
+  get 'items/show/:id', to: 'items#show'
 #  devise_for :users
 
 #Mode simple
