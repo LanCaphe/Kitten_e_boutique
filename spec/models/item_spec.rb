fe43463934d8e2cdb7019e2c3f51f624c950238f
Manon Lan <manondoublier@yahoo.fr>
2018-08-28 13:47:42 +0200

    tests fonctionnels

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,34 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  kitten{Items.new}
-     it "is valid with valid attributes" do
-         kitten.title = "Bob"
-         kitten.description = "the cat"
-         kitten.price= 10.99
-         kitten.image_url = "image"
-         expect(kitten).to be_valid
-       end
-
+ item=Item.new
     it "is not valid without a title" do
-      expect(kitten).to_not be_valid
+      expect(item).to_not be_valid
       end
 
     it "is not valid without a description" do
-      kitten.title = "bob"
-      expect(kitten).to_not be_valid
+      item.title = "bob"
+      expect(item).to_not be_valid
     end
 
     it "is not valid without a price" do
-      kitten.title = "Bob"
-      kitten.description = "Lorem ipsum dolor sit amet"
-      expect(kitten).to_not be_valid
+      item.title = "Bob"
+      item.description = "Lorem ipsum dolor sit amet"
+      expect(item).to_not be_valid
     end
 
     it "is not valid without a image_url" do
-      kitten.title = "Bob"
-      kitten.description = "Lorem ipsum dolor sit amet"
-      kitten.price = 10.26
-      expect(kitten).to_not be_valid
+      item.title = "Bob"
+      item.description = "Lorem ipsum dolor sit amet"
+      item.price = 10.26
+      expect(item).to_not be_valid
     end
-end 
+end
