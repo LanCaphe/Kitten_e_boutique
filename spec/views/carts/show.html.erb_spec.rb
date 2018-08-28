23e0746f4eb705b862320283aaf5837d2d07b690
ticho <tibofolly@gmail.com>
2018-08-28 17:00:22 +0200

    show cart - tests not written

--- spec/views/carts/show.html.erb_spec.rb
+++ spec/views/carts/show.html.erb_spec.rb
@@ -3,16 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe "carts/show.html.erb", type: :view do
-  describe "with a logged in user" do
-    fixtures :users
-    it "shows the proper items" do
-      user = users(:one)
-      item = Item.create(
-        title: "lol",
-        description: "haha",
-        price: 2
-      )
-      user.items.push(item)
-    end
-  end
+  # describe "with a logged in user" do
+  #   fixtures :users
+  #   it "shows the proper items" do
+  #     user = users(:one)
+  #     item = Item.create(
+  #       title: "lol",
+  #       description: "haha",
+  #       price: 2
+  #     )
+  #     user.items.push(item)
+  #   end
+  # end
 end
