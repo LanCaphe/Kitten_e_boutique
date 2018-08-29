4031ff0b28fe6803eca1c159c79977540f17b034
ticho <tibofolly@gmail.com>
2018-08-29 16:30:10 +0200

    login keeps cart

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,14 @@ Rails.application.routes.draw do
   get 'items/show/:id', to: 'items#show'
 
 #  devise_for :users
+  # post '/login', to: 'session/sessions#create'
 
 #Mode simple
-    devise_for :users, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
+    
+    # devise_for :users, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :users, controllers: {
+        sessions: 'session/sessions'
+      }
 end
