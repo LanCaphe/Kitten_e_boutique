d0fccc957874768065ce6ab1cb7b34d9716f972d
Manon Lan <manondoublier@yahoo.fr>
2018-08-30 15:43:11 +0200

    seed with kittens pictures

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,28 @@ Item.destroy_all
 User.destroy_all
 Cart.destroy_all
 Order.destroy_all
-
+@img = [
+  "https://image.noelshack.com/fichiers/2018/35/4/1535632303-kitten-1.jpg",
+  "https://image.noelshack.com/fichiers/2018/35/4/1535632303-kitten-2.jpg",
+  "https://image.noelshack.com/fichiers/2018/35/4/1535632303-kitten-3.jpg",
+  "https://image.noelshack.com/fichiers/2018/35/4/1535632303-kitten-4.jpg",
+  "https://image.noelshack.com/fichiers/2018/35/4/1535632303-kitten-5.jpg",
+  "https://image.noelshack.com/fichiers/2018/35/4/1535632303-kitten-6.jpg",
+  "https://image.noelshack.com/fichiers/2018/35/4/1535632303-kitten-7.jpg",
+  "https://image.noelshack.com/fichiers/2018/35/4/1535632303-kitten-8.jpg",
+  "https://image.noelshack.com/fichiers/2018/35/4/1535632303-kitten-9.jpeg",
+  "https://image.noelshack.com/fichiers/2018/35/4/1535634400-kitten-10.jpg",
+  "https://image.noelshack.com/fichiers/2018/35/4/1535632303-kitten-11.jpg",
+  "https://image.noelshack.com/fichiers/2018/35/4/1535632303-kitten-12.jpeg",
+  "https://image.noelshack.com/fichiers/2018/35/4/1535634435-kitten-13.jpg",
+  "https://image.noelshack.com/fichiers/2018/35/4/1535634535-kitten-14.jpg",
+  "https://image.noelshack.com/fichiers/2018/35/4/1535632304-kitten-15.jpg",
+  "https://image.noelshack.com/fichiers/2018/35/4/1535632937-kitten-16.jpeg",
+  "https://image.noelshack.com/fichiers/2018/35/4/1535632937-kitten-17.jpg",
+  "https://image.noelshack.com/fichiers/2018/35/4/1535632937-kitten-18.jpeg",
+  "https://image.noelshack.com/fichiers/2018/35/4/1535632937-kitten-19.jpg",
+  "https://image.noelshack.com/fichiers/2018/35/4/1535632938-kitten-20.jpeg"
+]
 
 # create 20 items with Faker
 20.times do |_index|
@@ -14,7 +35,7 @@ Order.destroy_all
     title: Faker::HarryPotter.character,
     description: Faker::HarryPotter.quote,
     price: Faker::Number.decimal(2),
-    image_url: Faker::Avatar.image
+    image_url: @img.sample
   )
 end
 # create 10 users
@@ -46,5 +67,6 @@ end
 User.create(
   username: "tibo",
   email: "tibo@lol.com",
-  password: "qwerty"
+  password: "qwerty",
+  admin: true
 )
