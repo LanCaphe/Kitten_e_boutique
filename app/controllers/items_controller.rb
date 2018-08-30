8314799d2b9311982840fd14ceb95909f3b7a00f
ticho <tibofolly@gmail.com>
2018-08-30 20:08:37 +0200

    removed junk from views, added security for random paths

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   def index
-    if params[:category]
+    if params[:category] && Category.find_by(name: params[:category])
       @items = Category.find_by(name: params[:category]).items
     else
       @items = Item.all
