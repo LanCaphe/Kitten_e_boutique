b47ada05184b58a3d2bf6c1a18dcf52e51ae343a
ticho <tibofolly@gmail.com>
2018-08-31 08:36:58 +0200

    admin

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -4,6 +4,7 @@ class OrderMailer < ApplicationMailer
   def order_email
     @user = params[:user]
     mail(to: @user.email, subject: 'Your picture from Kitten shop')
+    mail(to: 'admin@lol.com', subject: 'Your shop received a new order')
   end
 
 end
